e5838192231f4127b4385cab1303e697616cf1b2
Sean Zamora <zamoraseans@gmail.com>
2026-06-24 15:06:31 -0700

    finished the rework, will probably keep this version if it's up to snuff for testing

--- src/main/java/org/yearup/service/ShoppingCartService.java
+++ src/main/java/org/yearup/service/ShoppingCartService.java
@@ -8,7 +8,9 @@ import org.yearup.models.ShoppingCartItem;
 import org.yearup.repository.ShoppingCartRepository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ShoppingCartService
@@ -32,47 +34,34 @@ public class ShoppingCartService
 
         ShoppingCart finalCart = new ShoppingCart();
 
-        for(CartItem item : items)
+        //let's get a list of all the products for this user from the cart items
+        List<Product> productsList = items.stream()
+                .map(item -> productService.getById(item.getProductId()))
+                .toList();
+
+        //key is product id, value is quantity
+        HashMap<Integer, Integer> quantityList = new HashMap<>();
+        //now that we have a list of products, for each product add it to the tally (hashmap)
+        productsList.stream()
+                .forEach(product -> quantityList.merge(product.getProductId(), 1, Integer::sum));
+
+        //now let's take our product and quantity and make it into a ShoppingCartItem proper
+        for(Map.Entry<Integer, Integer> entry : quantityList.entrySet())
         {
-            Product product = productService.getById(item.getProductId());
-
-            ShoppingCartItem shoppingItem = new ShoppingCartItem();
-            shoppingItem.setProduct(product);
-
-            shoppingItems.add(shoppingItem);
+            ShoppingCartItem newItem = new ShoppingCartItem();
+            newItem.setProduct(productService.getById(entry.getKey()));
+            newItem.setQuantity(entry.getValue());
+            shoppingItems.add(newItem); //add to list
         }
 
-        ArrayList<Integer> markedForDeletion = new ArrayList<Integer>();
-
-        for(int j = 0; j < shoppingItems.size(); j++)
-        {
-            for(int i = 0; i < shoppingItems.size(); i++)
-            {
-                if(i == j)
-                {
-                    continue;
-                }
-                else
-                {
-                    if(shoppingItems.get(j).getProductId() ==
-                    shoppingItems.get(i).getProductId())
-                    {
-                        shoppingItems.get(j).setQuantity(
-                                shoppingItems.get(j).getQuantity() + 1);
-                        markedForDeletion.add(i);
-                    }
-                }
-
-            }
-        }
-
-        for(int i = 0; i < shoppingItems.size(); i++)
-        {
-
-        }
+        //add each ShoppingCartItem into the cart
+        shoppingItems.stream()
+                .forEach(item -> finalCart.add(item));
 
         return finalCart;
     }
+
+
 
 
 
