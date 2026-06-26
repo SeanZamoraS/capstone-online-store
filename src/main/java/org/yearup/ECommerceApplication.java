e1da31e0702d80e6f947ddc29b97de664288d5d5
Sean Zamora <zamoraseans@gmail.com>
2026-06-26 08:54:14 -0700

    messing around with banner

--- src/main/java/org/yearup/ECommerceApplication.java
+++ src/main/java/org/yearup/ECommerceApplication.java
@@ -10,7 +10,7 @@ public class ECommerceApplication
     public static void main(String[] args) {
         String dbName = System.getenv("DB_NAME");
         if (dbName != null && !dbName.isBlank()) {
-            String bannerResource = "classpath:banner-videogamestore" + dbName.toLowerCase() + ".txt";
+            String bannerResource = "classpath:banner-" + dbName.toLowerCase() + ".txt";
             System.setProperty("spring.banner.location", bannerResource);
         }
         SpringApplication.run(ECommerceApplication.class, args);
